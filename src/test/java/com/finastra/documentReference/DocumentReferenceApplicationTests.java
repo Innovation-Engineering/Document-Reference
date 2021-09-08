6ecb9d7484398b87b48afb53eb53d7f08073ad78
August Florese <aroueterra@gmail.com>
2021-09-08 19:09:56 +0800

    Init

---
+++ src/test/java/com/finastra/documentReference/DocumentReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finastra.documentReference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentReferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
